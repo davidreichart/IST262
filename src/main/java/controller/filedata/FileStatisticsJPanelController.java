f6c940c5731179cc3fecdf888c5edac77d8c721c
DavidReichart <david.s.eden@proton.me>
2024-10-08 04:55:35 -0400

    added method for creating metadata table in the GUI in FileStatisticsJPanelController

--- src/main/java/controller/filedata/FileStatisticsJPanelController.java
+++ src/main/java/controller/filedata/FileStatisticsJPanelController.java
@@ -45,16 +45,33 @@ public class FileStatisticsJPanelController {
                 if (node instanceof FileNode && ((FileNode) node).getSystemFile() instanceof ImageFile) {
                     node = (FileNode) node;
                     fileStatisticsJPanel.removeAll();
-
-                    JTable fileMetadataTable = new JTable(2, 1);
-                    fileMetadataTable.setValueAt("File Path: " + ((FileNode) node).getSystemFile().METADATA().absoluteFilePath(), 0, 0);
-                    fileMetadataTable.setValueAt("Pixel Count: " + ((ImageFile) ((FileNode) node).getSystemFile()).IMAGE_METADATA().pixelCount(), 1, 0);
-                    fileStatisticsJPanel.add(fileMetadataTable);
+                    fileStatisticsJPanel.add(imageMetadataTable((FileNode) node));
                     fileStatisticsJPanel.revalidate();
                 }
-
-
             }
         };
+    }
+
+    public JTable imageMetadataTable(FileNode node) {
+        // this JTable needs ImageFiles to get access to all metadata
+        if (!(node.getSystemFile() instanceof  ImageFile)) {
+            return null;
+        }
+
+        JTable fileMetadataTable = new JTable(5, 2);
+        fileMetadataTable.getColumnModel().getColumn(0).setMaxWidth(400); // the first column is the key
+
+        fileMetadataTable.setValueAt("File Path", 0, 0);
+        fileMetadataTable.setValueAt(node.getSystemFile().METADATA().absoluteFilePath(), 0, 1);
+
+        fileMetadataTable.setValueAt("File Size (kb)", 1, 0);
+        fileMetadataTable.setValueAt(node.getSystemFile().METADATA().kilobyteCount(), 1, 1);
+
+        fileMetadataTable.setValueAt("Image Dimensions", 2, 0);
+        fileMetadataTable.setValueAt(((ImageFile) node.getSystemFile()).IMAGE_METADATA().width() + " x " + ((ImageFile) node.getSystemFile()).IMAGE_METADATA().height(), 2, 1);
+
+        fileMetadataTable.setValueAt("Pixel Count", 3, 0);
+        fileMetadataTable.setValueAt(((ImageFile) node.getSystemFile()).IMAGE_METADATA().pixelCount(), 3, 1);
+        return fileMetadataTable;
     }
 }
