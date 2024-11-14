cd5da1638e8d948054bb5cc8b0447f3fc0208e17
DavidReichart <david.s.eden@proton.me>
2024-11-14 17:10:07 -0500

    fixed edit buttons on sorted file browser

--- src/main/java/controller/filedata/FileStatisticsJPanelController.java
+++ src/main/java/controller/filedata/FileStatisticsJPanelController.java
@@ -5,6 +5,8 @@ import model.data.filetypes.ImageFile;
 import model.data.filetypes.SystemFile;
 import model.util.FileInspector;
 import view.ApplicationJFrame;
+import view.filebrowser.FileBrowserJPanel;
+import view.filebrowser.SortedFileBrowserJPanel;
 import view.filebrowser.UserFileJTree;
 import view.filebrowser.nodes.DirectoryNode;
 import view.filebrowser.nodes.ImageNode;
@@ -64,6 +66,17 @@ public class FileStatisticsJPanelController {
         frame.getSortedFileBrowserJPanel()
                 .getFileList()
                 .addListSelectionListener(displaySortedFilePanelStatistics());
+        frame.getSortedFileBrowserJPanel()
+                .getFileList()
+                .addListSelectionListener(enableButtonsWhenItemIsSelected());
+        fileStatisticsJPanel.getEditFilePathButton()
+                .addActionListener(ALT_editFilePathButtonActionListener());
+        fileStatisticsJPanel.getEditFileSizeButton()
+                .addActionListener(ALT_editFileSizeButtonActionListener());
+        fileStatisticsJPanel.getEditImageHeightButton()
+                .addActionListener(ALT_editImageHeightButtonActionListener());
+        fileStatisticsJPanel.getEditImageWidthButton()
+                .addActionListener(ALT_editImageWidthButtonActionListener());
     }
 
     /**
@@ -278,6 +291,16 @@ public class FileStatisticsJPanelController {
         };
     }
 
+    public ListSelectionListener enableButtonsWhenItemIsSelected() {
+        return e -> {
+            if (frame.getSortedFileBrowserJPanel().getFileList().getSelectedValue() != null) {
+                enableAllEditButtons();
+            } else {
+                disableAllEditButtons();
+            }
+        };
+    }
+
     /**
      * Displays the metadata of the selected file in the FileStatisticsJPanel.
      * @return A ListSelectionListener that displays the metadata of the selected file in the FileStatisticsJPanel.
@@ -358,5 +381,257 @@ public class FileStatisticsJPanelController {
         fileStatisticsJPanel.add(fileStatisticsJPanel.getControlsPanel(), BorderLayout.WEST);
         fileStatisticsJPanel.add(imageMetadataTable(node), BorderLayout.CENTER);
         fileStatisticsJPanel.revalidate();
+    }
+
+    /**
+     * Action listener for {@link SortedFileBrowserJPanel} edit image width button.
+     * @return ActionListener for edit image width button.
+     */
+    private ActionListener ALT_editImageWidthButtonActionListener() {
+        return e -> {
+            // this listener only works on the SortedFileBrowser
+            Component current = this.frame.getTopFileAndBrowserPane().getLeftComponent();
+            if (current instanceof FileBrowserJPanel) {
+                return;
+            }
+            JList<String> list = frame.getSortedFileBrowserJPanel().getFileList();
+            int index = list.getSelectedIndex();
+            String selection = list.getSelectedValue();
+            if (selection == null) {
+                return;
+            }
+
+            // stream filter for selection
+            SystemFile selectedFile = context.getSystemFiles().stream()
+                    .filter(f -> f.METADATA().absoluteFilePath().equals(selection))
+                    .findFirst()
+                    .orElse(null);
+            if (selectedFile == null) {
+                // item doesnt exist, do nothing
+                return;
+            }
+
+            ImageFile imageFile = (ImageFile) selectedFile;
+            String input = JOptionPane.showInputDialog("Enter a new image width for this object:");
+
+            if (input.isEmpty()) {
+                JOptionPane.showMessageDialog(
+                        frame,
+                        "The image width cannot be empty.",
+                        "Invalid Image Width",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            // numeric / negative input check
+            for (char c : input.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    JOptionPane.showMessageDialog(
+                            frame,
+                            "The image width must only consist of numbers AND be a positive value",
+                            "Invalid Image Width",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+            }
+
+            ImageFile.ImageMetadata updatedMetadata = new ImageFile.ImageMetadata(Integer.parseInt(input), imageFile.IMAGE_METADATA().height(), imageFile.IMAGE_METADATA().colorHistogram());
+            imageFile.setIMAGE_METADATA(updatedMetadata);
+            // flick back and forth to trigger another event listener and update the table
+            if (index - 1 < 0) {
+                list.setSelectedIndex(index + 1);
+                list.setSelectedIndex(index);
+            } else {
+                list.setSelectedIndex(index - 1);
+                list.setSelectedIndex(index);
+            }
+        };
+    }
+
+    /**
+     * Action listener for {@link SortedFileBrowserJPanel} edit image height button.
+     * @return ActionListener for edit image height button.
+     */
+    private ActionListener ALT_editImageHeightButtonActionListener() {
+        return e -> {
+            // this listener only works on the SortedFileBrowser
+            Component current = this.frame.getTopFileAndBrowserPane().getLeftComponent();
+            if (current instanceof FileBrowserJPanel) {
+                return;
+            }
+            JList<String> list = frame.getSortedFileBrowserJPanel().getFileList();
+            int index = list.getSelectedIndex();
+            String selection = list.getSelectedValue();
+            if (selection == null) {
+                return;
+            }
+
+            // stream filter for selection
+            SystemFile selectedFile = context.getSystemFiles().stream()
+                    .filter(f -> f.METADATA().absoluteFilePath().equals(selection))
+                    .findFirst()
+                    .orElse(null);
+            if (selectedFile == null) {
+                // item doesnt exist, do nothing
+                return;
+            }
+
+            ImageFile imageFile = (ImageFile) selectedFile;
+            String input = JOptionPane.showInputDialog("Enter a new image height for this object:");
+
+            if (input.isEmpty()) {
+                JOptionPane.showMessageDialog(
+                        frame,
+                        "The image height cannot be empty.",
+                        "Invalid Image Height",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            // numeric / negative input check
+            for (char c : input.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    JOptionPane.showMessageDialog(
+                            frame,
+                            "The image height must only consist of numbers AND be a positive value",
+                            "Invalid Image Height",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+            }
+
+            int newHeight = Integer.parseInt(input);
+
+            ImageFile.ImageMetadata updatedMetadata = new ImageFile.ImageMetadata(imageFile.IMAGE_METADATA().width(), newHeight, imageFile.IMAGE_METADATA().colorHistogram());
+            imageFile.setIMAGE_METADATA(updatedMetadata);
+
+            // flick back and forth to trigger another event listener and update the table
+            if (index - 1 < 0) {
+                list.setSelectedIndex(index + 1);
+                list.setSelectedIndex(index);
+            } else {
+                list.setSelectedIndex(index - 1);
+                list.setSelectedIndex(index);
+            }
+        };
+    }
+
+    /**
+     * Action listener for {@link SortedFileBrowserJPanel} edit file size button.
+     * @return ActionListener for edit file size button.
+     */
+    private ActionListener ALT_editFileSizeButtonActionListener() {
+        return e -> {
+            // this listener only works on the SortedFileBrowser
+            Component current = this.frame.getTopFileAndBrowserPane().getLeftComponent();
+            if (current instanceof FileBrowserJPanel) {
+                return;
+            }
+            JList<String> list = frame.getSortedFileBrowserJPanel().getFileList();
+            int index = list.getSelectedIndex();
+            String selection = list.getSelectedValue();
+            if (selection == null) {
+                return;
+            }
+
+            // stream filter for selection
+            SystemFile selectedFile = context.getSystemFiles().stream()
+                    .filter(f -> f.METADATA().absoluteFilePath().equals(selection))
+                    .findFirst()
+                    .orElse(null);
+            if (selectedFile == null) {
+                // item doesn't exist, do nothing
+                return;
+            }
+
+            ImageFile imageFile = (ImageFile) selectedFile;
+            String input = JOptionPane.showInputDialog("Enter a new file size for this object:");
+
+            if (input.isEmpty()) {
+                JOptionPane.showMessageDialog(
+                        frame,
+                        "The file size cannot be empty.",
+                        "Invalid File Size",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            // numeric / negative input check
+            for (char c : input.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    JOptionPane.showMessageDialog(
+                            frame,
+                            "The file size must only consist of numbers AND be a positive value",
+                            "Invalid File Size",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+            }
+
+            long newSize = Long.parseLong(input);
+
+            SystemFile.Metadata updatedMetadata = new SystemFile.Metadata(newSize, imageFile.METADATA().absoluteFilePath());
+            imageFile.setMETADATA(updatedMetadata);
+
+            // flick back and forth to trigger another event listener and update the table
+            if (index - 1 < 0) {
+                list.setSelectedIndex(index + 1);
+                list.setSelectedIndex(index);
+            } else {
+                list.setSelectedIndex(index - 1);
+                list.setSelectedIndex(index);
+            }
+        };
+    }
+
+    /**
+     * Action listener for {@link SortedFileBrowserJPanel} edit file path button.
+     * @return ActionListener for edit file path button.
+     */
+    private ActionListener ALT_editFilePathButtonActionListener() {
+        return e -> {
+            // this listener only works on the SortedFileBrowser
+            Component current = this.frame.getTopFileAndBrowserPane().getLeftComponent();
+            if (current instanceof FileBrowserJPanel) {
+                return;
+            }
+            JList<String> list = frame.getSortedFileBrowserJPanel().getFileList();
+            int index = list.getSelectedIndex();
+            String selection = list.getSelectedValue();
+            if (selection == null) {
+                return;
+            }
+
+            // stream filter for selection
+            SystemFile selectedFile = context.getSystemFiles().stream()
+                    .filter(f -> f.METADATA().absoluteFilePath().equals(selection))
+                    .findFirst()
+                    .orElse(null);
+            if (selectedFile == null) {
+                // item doesnt exist, do nothing
+                return;
+            }
+
+            ImageFile imageFile = (ImageFile) selectedFile;
+            String input = JOptionPane.showInputDialog("Enter a new file path for this object:");
+
+            if (input.isEmpty()) {
+                JOptionPane.showMessageDialog(
+                        frame,
+                        "The file path cannot be empty.",
+                        "Invalid File Path",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            SystemFile.Metadata updatedMetadata = new SystemFile.Metadata(imageFile.METADATA().byteCount(), input);
+            imageFile.setMETADATA(updatedMetadata);
+
+            // flick back and forth to trigger another event listener and update the table
+            if (index - 1 < 0) {
+                list.setSelectedIndex(index + 1);
+                list.setSelectedIndex(index);
+            } else {
+                list.setSelectedIndex(index - 1);
+                list.setSelectedIndex(index);
+            }
+        };
     }
 }
